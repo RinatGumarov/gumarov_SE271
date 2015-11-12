659d3083c1c1da6bee820c3f9ba1ab06e1483027
Tiran678 <tiran678@icloud.com>
2015-11-12 04:29:26 +0300

    first commit, tree type created, list of leafs

---
+++ huffman.fs
@@ -0,0 +1,17 @@
+﻿module HuffmanSort = 
+    type Tree = 
+        | Leaf of char * int
+        | Node of int * Tree * Tree
+
+    type HuffmanCompr(symbols: seq<char>, freq: seq<int>) =
+
+
+        let leafs =
+            Seq.zip symbols freq
+            |> Seq.toList 
+            |> List.map Leaf
+
+        let frequency node = 
+            match node with
+            | Leaf(_,p) -> p
+            | Node(p,_,_) -> p
