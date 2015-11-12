19246213d1ae5df60dda60d995f8e0bd3227dcd5
Tiran678 <tiran678@icloud.com>
2015-11-12 04:32:10 +0300

    module renamed

--- huffman.fs
+++ huffman.fs
@@ -1,16 +1,19 @@
-﻿module HuffmanSort = 
+﻿module HuffmanCompression = 
+// дерево
+// листья дерева хранят в себе символ и сколько раз этот символ встречается(частоту)
+// все остальные вершины хранят ссылки на поодеревья и сумму частот поддеревьев
     type Tree = 
         | Leaf of char * int
         | Node of int * Tree * Tree
 
     type HuffmanCompr(symbols: seq<char>, freq: seq<int>) =
 
-
+    // список из пар символ*частота(это и есть листья)
         let leafs =
             Seq.zip symbols freq
             |> Seq.toList 
             |> List.map Leaf
-
+   // getFreq
         let frequency node = 
             match node with
             | Leaf(_,p) -> p
