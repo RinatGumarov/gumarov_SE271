dc2fcb034a67d6989c7df38a06708202c188a495
Tiran678 <tiran678@icloud.com>
2015-11-12 04:53:20 +0300

    tree generator added

--- huffman.fs
+++ huffman.fs
@@ -14,7 +14,22 @@
             |> Seq.toList 
             |> List.map Leaf
    // getFreq
-        let frequency node = 
+        let freq node = 
             match node with
             | Leaf(_,p) -> p
             | Node(p,_,_) -> p
+
+
+            //создание дерева по списку из листьев
+            //нижний уровень - листья(символ,частота)
+            //сортируем элемент по частоте, чтобы выбрать два с наименьшей
+            //создаем новую вершину по принципу - вешины с наименьшей частотой - сыновья, частота = сумма частот сыновей
+
+
+        let rec buildTree roots =
+            match roots |> List.sortBy freq with
+            | [] -> failwith "Error! empty list"
+            | [node] -> node
+            | minmin::min::rest -> 
+                let newNode = Node(freq minmin + freq min, minmin, min)
+                buildTree (newNode::rest)
