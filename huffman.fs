e77c82412613491f7fb20f421881b0ff3a7d036d
Tiran678 <tiran678@icloud.com>
2015-12-08 17:37:47 +0300

    comments deleted

--- huffman.fs
+++ huffman.fs
@@ -15,10 +15,6 @@
         |_ -> x * (degree x (d-1))
 
 
-    let input = File.ReadAllText("/Users/Rinat/Movies/test.txt")
-    printfn "%A" input
-    let listOfFreqs = [for i in input -> (i, 1)]
-
     //перевод последовательности из 8 битов в число
     let binToDec (bin:int list) = [for i in 0 .. 7 -> bin.Item(i)*(if i = 7 then 1 else ([for j in 0 .. (6-i) -> 2] |> List.reduce(*)))] |> List.reduce(+)
 
@@ -48,15 +44,12 @@
         | [] -> []
       
     //получим список из пар (символ,частота) -> список из листьев
-    let leafs = otsev (List.rev (otsev2 listOfFreqs)) |> List.map (fun (x,y)->Leaf(x,y))
 
     let rec len x =
         match x/10 with
         | 0 -> 1
         | _ -> 1 + (len (x/10))
 
-    printfn "%A" [for i in 0 .. 9 -> i]
-   // getFreq
     let freq node = 
             match node with
             | Leaf(_,p) -> p
@@ -75,112 +68,6 @@
                 let newNode = Node(freq minmin + freq min, minmin, min)
                 buildTree (newNode::rest)               
         
-    let tree = buildTree leafs
-    printfn"%A" tree
-
-        // шифрование символов следующим образом
-       //           [корень]
-       //       (1) |     | (0)
-       //     [вершина]   [вершина]
-    let huffmanCodeTable =
-           let rec huffmanCodes tree =
-               match tree with
-               | Leaf (c,_) -> [(c,[])]
-               | Node (_, left, right) ->
-                   let leftCodes = huffmanCodes left |> List.map (fun (c, code) -> (c,1::code))
-                   let rightCodes = huffmanCodes right |> List.map (fun (c, code) -> (c,0::code))
-                   List.append leftCodes rightCodes
-           huffmanCodes tree
-           |> List.map (fun (c, code) -> (c, List.toArray code))
-           |> Map.ofList;
-    
-    
-    let encode (str : string) =
-            let encodeChar c =
-                match huffmanCodeTable |> Map.tryFind c with
-                | Some bits -> bits
-                | None -> failwith "No frequency info provided for character %c" c
-            str.ToCharArray()
-            |> Array.map encodeChar
-            |> Array.concat
-            |> Array.toList
-
-    // получим последовательность из нулей и единиц
-    let listOfBits = encode input
-
-    // получим последовательность из символов отрезав несколько бит с конца, чтобы делилось на 8
-    let listOfChars = ([for i in 0 .. ((listOfBits.Length)/8 - 1) -> copy listOfBits (i+7*i) 8]|> List.map binToDec|> List.map char ) @ [char(binToDec ((copy listOfBits (8*(listOfBits.Length/8)) (listOfBits.Length-(8*(listOfBits.Length/8))-1)) @ [for i in 0 .. listOfBits.Length%8 -> 0]))]
-    let rest0 = char (binToDec(decToBin(listOfBits.Length%8-1)))
-    printfn "%A" rest0
-
-
-    let str = new StreamWriter("/Users/Rinat/FSProjects/Huffman/output.enc")
-    str.Write(rest0)
-    for i in listOfChars do str.Write(i)
-    str.Close()
-
-
-    let treeStr = new StreamWriter("/Users/Rinat/FSProjects/Huffman/output.tree")
-    for i in 0 .. (leafs.Length-1) do treeStr.Write((fun node->match node with | Leaf(x,_)-> x | Node(_,_,_)->failwith "Expected Leaf, but here Node") (leafs.Item(i)))
-    treeStr.Write((fun node->match node with | Leaf(x,_)-> x | Node(_,_,_)->failwith "Expected Leaf, but here Node") (leafs.Item(0)))
-    for i in 0 .. (leafs.Length-1) do 
-                            treeStr.Write((fun node->match node with | Leaf(_,x)-> x | Node(_,_,_)->failwith "Expected Leaf, but here Node") (leafs.Item(i)))
-                            treeStr.Write(" ")
-    treeStr.Close()
-
-    let bitiki = File.ReadAllText("/Users/Rinat/FSProjects/Huffman/output.ri")|> Seq.toList |> List.map int 
-    printfn "%A" bitiki
-    let delete = bitiki.Item(0)-1
-    printfn "%A" delete
-    let ololo = [for i in 1 .. (bitiki.Length-1) -> (decToBin (bitiki.Item(i)))] |> List.concat
-    let bitbit = copy ololo 0 (ololo.Length-delete)
-    printfn "%A" bitbit
-//    File.WriteAllText("output.trn", (encode input))
-    
-      // восстановить исходный файл по списку из нулей и единиц  
-    let decode bits = 
-            let rec decodeInner bitsLeft treeNode result =
-                match bitsLeft, treeNode with
-                | [], Node(_,_,_) -> result |>List.rev |> List.toArray //failwith "Bits provided did not form a complete word"
-                | [], Leaf(c,_) -> (c::result) |>List.rev |> List.toArray
-                | _, Leaf(c,_) -> decodeInner bitsLeft tree (c::result)
-                | b::rest, Node(_,l,r) -> if (b=1)
-                                          then decodeInner rest l result
-                                          else decodeInner rest r result 
-            new string (decodeInner bits tree [])  
-    printfn "%A" (decode bitbit)
-
-    let leafs' = File.ReadAllText("/Users/Rinat/FSProjects/Huffman/output.tree").ToCharArray()|>Array.toList
-    let mutable i = 1 
-    let chars =(leafs'.Item(0))::(seq{
-        while not (leafs'.Item(0).Equals(leafs'.Item(i))) do
-         yield leafs'.Item(i)
-         i <- i+1
-        }|>Seq.toList)
-    i <- i+1
-    let rec num (x: int list) =
-     match x with
-     |[a] -> a 
-     |[x;y] -> 10*x+y
-     |(l::r::ls) -> num ((10*l+r)::ls)
-     |_ -> 0
-    printfn "%A" (num [7;6;0;7;7])
-    let freqs' = (seq{
-        while i < (leafs'.Length-1) do
-            yield (seq{
-                while not (' '.Equals(leafs'.Item(i))) do
-                    yield leafs'.Item(i)
-                    i <- i + 1
-            }|> Seq.toList)|>List.map (fun x -> match x with |'0'->0|'1'->1|'2'->2|'3'-> 3|'4'->4|'5'->5|'6'->6|'7'->7|'8'->8|'9'->9|_->0)
-            i<-i+1
-        }|> Seq.toList) |> List.map num
-    printfn "%A" chars
-    printfn "%A" freqs'
-    let leafsololo = List.zip chars freqs' |> List.map (fun (x,y)->Leaf(x,y))
-    printfn"%A" leafsololo
-    let newTree = buildTree leafsololo
-    printfn"%A" newTree
-
     let rec archive (inputPath: string) (outputPath:string) (x: char) = 
         match x with
         |'-' -> 
@@ -259,12 +146,12 @@
                                               then decodeInner rest l result
                                               else decodeInner rest r result 
                 new string (decodeInner bits tree []) 
-            let delete = bitiki.Item(0)-1
-            let ololo = [for i in 1 .. (bitiki.Length-1) -> (decToBin (bitiki.Item(i)))] |> List.concat
+            let delete = input.Item(0)-1
+            let ololo = [for i in 1 .. (input.Length-1) -> (decToBin (input.Item(i)))] |> List.concat
             let bitbit = copy ololo 0 (ololo.Length-delete)
             let str = new StreamWriter(outputPath)
             for i in (decode bitbit) do str.Write(i)
             str.Close()
         | _ -> printfn"use:: inputPath outputPath x(+ -> decompress; - -> compress)" 
 //    archive "/Users/Rinat/Movies/test.txt" "/Users/Rinat/Movies/modif" '-'
-//    archive "/Users/Rinat/Movies/modif" "/Users/Rinat/Movies/ololo.txt" '+'
+    archive "/Users/Rinat/Movies/modif" "/Users/Rinat/Movies/ololo.txt" '+'
